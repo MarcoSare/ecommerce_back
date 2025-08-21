445954bf5630fc652db7f2c951b7b344ed9006ee
MarcoSare <marcoramirez3700@gmail.com>
2025-08-21 01:55:43 -0600

    fixed pedidos response

--- msv-pedidos/src/main/java/com/ecommerce/msvpedidos/mappers/PedidoMapper.java
+++ msv-pedidos/src/main/java/com/ecommerce/msvpedidos/mappers/PedidoMapper.java
@@ -1,15 +1,18 @@
 package com.ecommerce.msvpedidos.mappers;
 
+import com.ecommerce.commons.dto.ClientesResponse;
 import com.ecommerce.commons.dto.PedidosRequest;
 import com.ecommerce.commons.dto.PedidosResponse;
 import com.ecommerce.commons.dto.ProductoResponse;
 import com.ecommerce.commons.mappers.CommonMapper;
+import com.ecommerce.msvpedidos.clients.ClientesClients;
 import com.ecommerce.msvpedidos.clients.ProductoClient;
 import com.ecommerce.msvpedidos.entities.Pedido;
 import com.ecommerce.msvpedidos.entities.Producto;
 import com.ecommerce.msvpedidos.entities.ProductoPedido;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -19,9 +22,11 @@ import java.util.Set;
 public class PedidoMapper extends CommonMapper<PedidosRequest, PedidosResponse, Pedido> {
 
     private ProductoClient productoClient;
+    private ClientesClients clientesClients;
 
-    public PedidoMapper(ProductoClient productoClient) {
+    public PedidoMapper(ProductoClient productoClient,  ClientesClients clientesClients) {
         this.productoClient = productoClient;
+        this.clientesClients = clientesClients;
     }
 
     @Override
@@ -55,6 +60,10 @@ public class PedidoMapper extends CommonMapper<PedidosRequest, PedidosResponse, 
         if (entity == null) {
             return null;
         }
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String fechaFormateada = formatter.format(entity.getFechaCreacion());
+        ClientesResponse clientesResponse = clientesClients.getClienteById(entity.getIdCliente());
+        String cliente = clientesResponse.nombre() + " " +  clientesResponse.apellido();
 
 
         double total = entity.getProductos()
@@ -81,8 +90,9 @@ public class PedidoMapper extends CommonMapper<PedidosRequest, PedidosResponse, 
         PedidosResponse response = new PedidosResponse(
                 entity.getId(),
                 entity.getIdCliente(),
+                cliente,
                 total,
-                entity.getFechaCreacion(),
+                fechaFormateada,
                 entity.getEstado(),
                 productos
         );
