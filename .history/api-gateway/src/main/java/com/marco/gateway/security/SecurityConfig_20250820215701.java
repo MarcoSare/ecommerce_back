776171a217fc81bb19138ee98930aecec9d54198
WilbertJimenez <wilbert.jimenezmarin@gmail.com>
2025-08-20 22:02:03 -0600

    Commit del master

---
+++ .history/api-gateway/src/main/java/com/marco/gateway/security/SecurityConfig_20250820215701.java
@@ -0,0 +1,54 @@
+package com.marco.gateway.security;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    	http.csrf(csrf -> csrf.disable())
+    			.cors(cors -> cors.configurationSource(request -> {
+    				CorsConfiguration config = new CorsConfiguration();
+    				config.setAllowedOrigins(List.of("http://localhost:4200"));
+    				config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    				config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    				config.setAllowCredentials(true);
+    				return config;
+    			})).authorizeExchange(ex -> ex
+    		            .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+    		            .pathMatchers(HttpMethod.GET, "/**").permitAll()//hasAnyRole("ADMIN", "USER")
+    		            .pathMatchers(HttpMethod.POST, "/**").permitAll()//hasAnyRole("ADMIN", "USER")
+    		            .pathMatchers(HttpMethod.PUT, "/**").permitAll() //hasRole("ADMIN")
+    		            .pathMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+    		            .anyExchange().authenticated()
+    		        )
+    		        .oauth2ResourceServer(oauth2 -> oauth2
+    		            .jwt(jwt -> jwt.jwtAuthenticationConverter(reactiveJwtAuthenticationConverter()))
+    		        );
+    	return http.build();
+    }
+    
+    @Bean
+    ReactiveJwtAuthenticationConverterAdapter reactiveJwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+        authoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtConverter);
+    }
+}
