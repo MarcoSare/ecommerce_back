e299a346364b2a05b71e726d3fec5634920d8427
AndresFG0808 <floresgarcialuisandres409@gmail.com>
2025-08-20 22:19:44 -0600

    Cambios en 'PedidosServiceImpl'

--- msv-pedidos/src/main/java/com/ecommerce/msvpedidos/services/PedidosServiceImpl.java
+++ msv-pedidos/src/main/java/com/ecommerce/msvpedidos/services/PedidosServiceImpl.java
@@ -83,9 +83,10 @@ public class PedidosServiceImpl implements PedidosService{
 		return mapper.entityToResponse(pedido);
 	}
 	
+	
 	@Override
-	@Transactional(readOnly = true)
-	public int countByClienteId(Long clienteId) {
-		return repository.existsByIdCliente(clienteId);
+	public int countByClienteId(Long id) {
+		return repository.existsByIdCliente(id);
 	}
+	
 }
